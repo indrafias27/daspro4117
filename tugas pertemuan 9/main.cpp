eb36ba0cec1096795fe3d2e9bffc483e349489c6
indrafias27 <112920532+indrafias27@users.noreply.github.com>
2022-10-10 00:00:53 +0700

    Add files via upload

---
+++ tugas pertemuan 9/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Tugas DasPro ke 9" << endl;
+    cout << "MEncari angka kelipatan 5" << endl;
+
+    int a;
+
+    a = 5;
+    while (a <= 100){
+        cout << a << endl;
+        a+=5;
+    }
+    return 0;
+}
